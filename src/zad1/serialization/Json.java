58a1a8d8a3abacde56851c70cc27403ef0b3884e
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-05-10 23:19:03 +0200

    unserializeArrayOfStrings

--- src/zad1/serialization/Json.java
+++ src/zad1/serialization/Json.java
@@ -1,9 +1,13 @@
 package zad1.serialization;
 
 public class Json {
-    public static String serializeArrayOfStrings(String[] stringArray) {
+    public static String serializeArrayOfStrings(String[] array) {
         return "[\""
-                + String.join("\",\"", stringArray)
+                + String.join("\",\"", array)
                 + "\"]";
+    }
+
+    public static String[] unserializeArrayOfStrings(String serializedArray) {
+        return serializedArray.trim().replaceAll("[\\[\"\\]]", "").split(",");
     }
 }
