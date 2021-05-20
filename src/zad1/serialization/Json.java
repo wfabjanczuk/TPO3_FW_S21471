c3ca77cfeeef5fd13c24310a56a157f8256c0e17
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-05-21 00:06:30 +0200

    Serialization refactored

--- src/zad1/serialization/Json.java
+++ src/zad1/serialization/Json.java
@@ -2,29 +2,40 @@ package zad1.serialization;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Json {
     public static String serializeString(String text) {
-        return '"' + text + '"';
+        return '"' + text.replaceAll("\"", "\\\"").replaceAll("\\s", "_") + '"';
     }
 
     public static String unserializeString(String serializedString) {
-        return serializedString.trim().replaceAll("\"", "");
+        return serializedString.substring(1, serializedString.length() - 1)
+                .replaceAll("\\\"", "\"")
+                .replaceAll("_", " ");
     }
 
-    public static String serializeStrings(String ...strings) {
-        return "[\""
-                + String.join("\",\"", strings)
-                + "\"]";
+    public static String serializeStrings(String... strings) {
+        return serializeStrings(Arrays.asList(strings));
     }
 
-    public static String serializeStrings(Iterable<? extends CharSequence> strings) {
-        return "[\""
-                + String.join("\",\"", strings)
-                + "\"]";
+    public static String serializeStrings(List<String> strings) {
+        List<String> escapedStrings = strings.stream().map(Json::serializeString).collect(Collectors.toList());
+
+        return "[" + String.join(",", escapedStrings) + "]";
     }
 
     public static List<String> unserializeStrings(String serializedStrings) {
-        return Arrays.asList(serializedStrings.trim().replaceAll("[\\[\"\\]]", "").split(","));
+        if (serializedStrings.length() < 4) {
+            throw new IllegalArgumentException("Invalid string to unserialize");
+        }
+
+        Stream<String> stringStream = Arrays.stream(serializedStrings
+                .substring(1, serializedStrings.length() - 1)
+                .split("(?<=\"),(?=\")")
+        );
+
+        return stringStream.map(Json::unserializeString).collect(Collectors.toList());
     }
 }
